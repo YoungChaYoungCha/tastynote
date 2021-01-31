d3e152ceb1c79cd76f2707bcce798e6a5dbe3c8b
ChaeByunghoon <cbh1203@naver.com>
2021-02-01 00:06:39 +0900

    이니셜 커밋

---
+++ src/main/java/com/youngchayoungcha/tastynote/TastynoteApplication.java
@@ -0,0 +1,13 @@
+package com.youngchayoungcha.tastynote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastynoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastynoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youngchayoungcha/tastynote/TastynoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngchayoungcha.tastynote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastynoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
